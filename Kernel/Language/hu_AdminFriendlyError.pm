79174378ea5cb22b696d1b7258a7559290e9e7b3
Balázs Úr <urbalazs@gmail.com>
2016-11-28 23:33:36 +0100

    Added Hungarian translation and small fixes

--- Kernel/Language/hu_AdminFriendlyError.pm
+++ Kernel/Language/hu_AdminFriendlyError.pm
@@ -1,13 +1,14 @@
 # --
-# Kernel/Language/de_AdminFriendlyError.pm - the German translation of AdminFriendlyError
+# Kernel/Language/hu_AdminFriendlyError.pm - the Hungarian translation of AdminFriendlyError
 # Copyright (C) 2016 Perl-Services.de, http://perl-services.de/
+# Copyright (C) 2016 Balázs Úr, http://www.otrs-megoldasok.hu
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Language::de_AdminFriendlyError;
+package Kernel::Language::hu_AdminFriendlyError;
 
 use strict;
 use warnings;
@@ -20,12 +21,13 @@ sub Data {
 
     # Custom/Kernel/Output/HTML/Templates/Standard/Error.tt
     $Lang->{'This error might be caused by a bug. Please inform the administrator (%s) about this problem.'} =
-        'Dieser Fehler könnte durch einen Bug verursacht sein. Bitte informieren Sie Ihren Admin (%s) über dieses Problem.';
+        'Ezt a problémát egy szoftverhiba is okozhatta. Kérjük, hogy tájékoztassa a rendszergazdát (%s) erről a problémáról!';
     $Lang->{'The information in \'Error Details\' can help in locating the problem, so please send them to the administrator.'} =
-        'Die Informationen unter \'Fehlerdetails\' können dem Administrator helfen das Problem zu finden. Bitte schicken Sie die Informationen an den Administrator.';
+        'A „Hiba részleteiben” lévő információk segíthetnek azonosítani a problémát, ezért arra kérjük, hogy küldje el azokat a rendszergazdának.';
 
     # Kernel/Config/Files/AdminFriendlyError.xml
-    $Lang->{'Defines the mail address where agents can send bug reports to.'} = '';
+    $Lang->{'Defines the mail address where agents can send bug reports to.'} =
+        'Meghatározza azt az e-mail címet, ahova az ügyintézők a hibajelentéseket küldhetik.';
 }
 
 1;
